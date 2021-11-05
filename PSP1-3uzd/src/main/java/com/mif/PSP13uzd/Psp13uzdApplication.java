4a79e96ea65f995bba5e3d4243fd458028e2fb93
Grantas Bičiulaitis <grantas.biciulaitis@gmail.com>
2021-11-05 22:06:43 +0200

    Created empty Spring project

---
+++ PSP1-3uzd/src/main/java/com/mif/PSP13uzd/Psp13uzdApplication.java
@@ -0,0 +1,13 @@
+package com.mif.PSP13uzd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Psp13uzdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Psp13uzdApplication.class, args);
+	}
+
+}
---
+++ PSP1-3uzd/src/test/java/com/mif/PSP13uzd/Psp13uzdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mif.PSP13uzd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Psp13uzdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
